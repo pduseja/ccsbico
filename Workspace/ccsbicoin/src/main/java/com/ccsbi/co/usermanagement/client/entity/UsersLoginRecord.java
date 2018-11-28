be21a4365e2eb2cae60892c2cfad36ee02a73651
pduseja <dusejapt@gmail.com>
2018-11-28 16:44:07 +0000

    Update UsersLoginRecord.java
    correct the file

--- Workspace/ccsbicoin/src/main/java/com/ccsbi/co/usermanagement/client/entity/UsersLoginRecord.java
+++ Workspace/ccsbicoin/src/main/java/com/ccsbi/co/usermanagement/client/entity/UsersLoginRecord.java
@@ -12,7 +12,7 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(description = "Login")
 @JacksonXmlRootElement(localName = "Login")
 @SuppressWarnings("serial")
-public class Login implements Serializable{
+public class UsersLoginRecord implements Serializable{
 
 
 	@ApiModelProperty(name = "userName", value = "userName")
@@ -28,8 +28,30 @@ public class Login implements Serializable{
 	@ApiModelProperty(name = "rememberMe", value = "rememberMe")
 	@JsonProperty("rememberMe")
 	@JacksonXmlProperty(localName = "rememberMe") 
-	private String rememberMe;
+	private Boolean rememberMe;
+	
+	@ApiModelProperty(name = "token", value = "token")
+	@JsonProperty("token")
+	@JacksonXmlProperty(localName = "token") 
+	private String token;
 
+	
+	@ApiModelProperty(name = "name", value = "name")
+	@JsonProperty("name")
+	@JacksonXmlProperty(localName = "name")
+	private String name;
+	
+	@ApiModelProperty(name = "cookie", value = "cookie")
+	@JsonProperty("cookie")
+	@JacksonXmlProperty(localName = "cookie")
+	private String cookie;
+	
+	@ApiModelProperty(name = "cookieExpirytime", value = "cookieExpirytime")
+	@JsonProperty("cookieExpirytime")
+	@JacksonXmlProperty(localName = "cookieExpirytime")
+	private int cookieExpirytime;
+	
+	
 	/**
 	 * @return the userName
 	 */
@@ -59,18 +81,75 @@ public class Login implements Serializable{
 	}
 
 	/**
+	 * @return the token
+	 */
+	public String getToken() {
+		return token;
+	}
+
+	/**
+	 * @param token the token to set
+	 */
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	/**
 	 * @return the rememberMe
 	 */
-	public String getRememberMe() {
+	public Boolean getRememberMe() {
 		return rememberMe;
 	}
 
 	/**
 	 * @param rememberMe the rememberMe to set
 	 */
-	public void setRememberMe(String rememberMe) {
+	public void setRememberMe(Boolean rememberMe) {
 		this.rememberMe = rememberMe;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the cookie
+	 */
+	public String getCookie() {
+		return cookie;
+	}
+
+	/**
+	 * @param cookie the cookie to set
+	 */
+	public void setCookie(String cookie) {
+		this.cookie = cookie;
+	}
+
+	/**
+	 * @return the cookieExpirytime
+	 */
+	public int getCookieExpirytime() {
+		return cookieExpirytime;
+	}
+
+	/**
+	 * @param cookieExpirytime the cookieExpirytime to set
+	 */
+	public void setCookieExpirytime(int cookieExpirytime) {
+		this.cookieExpirytime = cookieExpirytime;
 	}
 	
 	
 }
+
